526058efa3a8888873703cb1a091df62a6fa46de
chloechagot <chloechagot@hotmail.fr>
2017-03-13 23:55:41 +0100

    vue des articles

--- affichage/header.php
+++ affichage/header.php
@@ -16,24 +16,26 @@
 
 
 <header class="header" id="main-header">
-    
-    
-                        <!--Menu Hamburger de flûte ! -->
-    
+
+
+    <!--Menu Hamburger de flûte ! -->
+
     <div class="header-toogle">
-    <a class="header-toogle-open" href="#main-header">&#9776;</a>
-    <a class="header-toogle-close" href="#">&#735;</a>
+        <a class="header-toogle-open" href="#main-header">&#9776;</a>
+        <a class="header-toogle-close" href="#">&#735;</a>
     </div>
-    
-                        <!-- Menu principal -->
+
+    <!-- Menu principal -->
     <div class="header-menu">
-        <div class="box accueil"><a href="#" title="Accueil" target="_blank">Accueil</a></div>
-        <div class="box articless"><a href="#" title="Articles" target="_blank">Articles</a></div>
-         <div class="box logo"><a href="#" title="logo" target="_blank" class="logos"><img src="../img/logo-officiel.png" alt="Logo"></a></div>
-         <div class="box contact"><a href="#" title="Contact" target="_blank">Contact</a></div>
-         <div class="box repertoire"><a href="#" title="Ecoles" target="_blank">Répertoire des écoles</a></div>
+        <div class="box accueil"><a href="accueil.php" title="Accueil" target="_blank">Accueil</a></div>
+        <div class="box articles"><a href="articles.php" title="Articles" target="_blank">Articles</a></div>
+        <div class="box logo">
+            <a href="accueil.php" title="logo" target="_blank" class="logos"><img src="../img/logo-officiel.png" alt="Logo"></a>
+        </div>
+        <div class="box contact"><a href="#" title="Contact" target="_blank">Contact</a></div>
+        <div class="box repertoire"><a href="ecoles.php" title="Ecoles" target="_blank">Répertoire des écoles</a></div>
     </div>
-<div class="ligne"></div>
+    <div class="ligne"></div>
 </header>
 
 <!--
